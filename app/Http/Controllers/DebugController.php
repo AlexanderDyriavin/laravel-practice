c9ae23c19ab294ac0ef90502b8a5f188517f461f
dyriavin <Dyriavin@gmail.com>
2020-09-02 00:45:51 +0300

    Implemeted the seed the database updated composer.json for auto-loading

--- app/Http/Controllers/DebugController.php
+++ app/Http/Controllers/DebugController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Company;
+use App\User;
 use Illuminate\Http\Request;
 
 class DebugController extends Controller
@@ -10,5 +11,10 @@ class DebugController extends Controller
     public function generate()
     {
         factory(Company::class,20);
+        factory(User::class);
+        factory(User::class);
+
+        return response(['status'=> 'ok']);
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 //Route::get('/','CityController@index');
-//Route::get('/',formatAction('DebugController','generate'));
+Route::get('/',formatAction('DebugController','generate'));
