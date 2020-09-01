9f712058a52710faf734e73bcdc6d8ec113e8c1a
dyriavin <Dyriavin@gmail.com>
2020-09-02 00:53:18 +0300

    Created full factories for all models updated events table

--- app/Http/Controllers/DebugController.php
+++ app/Http/Controllers/DebugController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
 use App\Models\Company;
+use App\Models\Event;
+use App\Models\Person;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -11,8 +14,10 @@ class DebugController extends Controller
     public function generate()
     {
         factory(Company::class,20);
-        factory(User::class);
-        factory(User::class);
+        factory(User::class,20);
+        factory(City::class,20);
+        factory(Event::class,20);
+        factory(Person::class,20);
 
         return response(['status'=> 'ok']);
 
