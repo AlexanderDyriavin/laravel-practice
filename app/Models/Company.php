1de2c244dbde228d128b829f130b3e1737061073
dyriavin <Dyriavin@gmail.com>
2020-09-04 23:40:24 +0300

    Created relation between models updated php-doc

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -3,12 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * Class Company
+ * @package App\Models
+ */
 class Company extends Model
 {
+    /**
+     * @var string[]
+     */
     protected $fillable = [
         'name',
         'address',
         'phone'
     ];
+
+    /**
+     * @return HasMany
+     */
+    public function employe(): HasMany
+    {
+        return $this->hasMany(Person::class);
+    }
 }
